3d642acb282825a898ad577e493fd4ac7cffbc96
Roberto Pereira <125472287+RobertoNeto96@users.noreply.github.com>
2026-05-14 15:57:45 -0300

    Fazendo os primeiros desafios
    Fazendo do desafio 1 ao desafio 3

--- Anotações/Anotaçoes.py
+++ Anotações/Anotaçoes.py
@@ -1,2 +1,40 @@
 # Anotaçoes do curso de PYTHON
 
+# str = string (Define como 'escrita' o valor)
+# int = inteiro (Define o valor com um numero INTEIRO) 
+# float = flutuante (Define o valor como numero com virgula/ponto)
+# bool = boleano (Define o valor como TRUE ou FALSE)
+
+# INPUT é o comando para deixar o programa interativo, onde o usuario vai precisar digitar a informação
+
+# 
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
---
+++ Desafios/Desafio 3.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero: '))
+
+print(f'A soma dos numeros é {n1 + n2}')
