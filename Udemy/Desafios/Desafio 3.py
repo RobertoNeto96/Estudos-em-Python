e695b5efe07bb08a37037b584ffb358e7244e22e
Roberto Pereira <125472287+RobertoNeto96@users.noreply.github.com>
2026-06-11 17:27:15 -0300

    praticando TRY/EXCEPT

---
+++ Udemy/Desafios/Desafio 3.py
@@ -0,0 +1,20 @@
+while True:
+    try:
+        entrada = int(input('Digite um numero inteiro: '))
+
+        numero = int(entrada)
+
+        if numero % 2 == 0:
+            print('O numero digitado é PAR')
+            cont = str(input('Quer verificar outro numero? [S/N]' )).upper()
+            if cont == 'N':
+                print('Obrigado por usar nosso sistema, volte sempre!')
+                break
+        else:
+            print('O numero digitado é IMPAR')
+            cont = str(input('Quer verificar outro numero? [S/N]' )).upper()
+            if cont == 'N':
+                print('Obrigado por usar nosso sistema, volte sempre!')
+                break
+    except ValueError:
+        print('Numero invalido, digite um numero sem pontos ou virgulas.')            
